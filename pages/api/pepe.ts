fc3d59d745ab0b0b26ee88a2fa3af26410938791
ghostframe <marcosandresdiaz@gmail.com>
2022-11-10 17:49:46 -0300

    test endpoint

--- pages/api/pepe.ts
+++ pages/api/pepe.ts
@@ -4,6 +4,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<{ accessToken: string }>
 ) {
-  res.status(200).end();
   setTimeout(() => fetch('https://pruebaaaaaaaa.requestcatcher.com'), 5000);
+  res.status(200).end();
 }
