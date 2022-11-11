e86290729b8c25a1e2e75514a831a4c69a648076
ghostframe <marcosandresdiaz@gmail.com>
2022-11-11 09:02:33 -0300

    remove test endpoint

--- pages/api/pepe.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<{ accessToken: string }>
-) {
-  setTimeout(() => fetch('https://pruebaaaaaaaa.requestcatcher.com'), 5000);
-  res.status(200).end();
-}
